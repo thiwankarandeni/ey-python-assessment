23de01da9efe1ff89be36d981db00f206edc414e
thiwankarandeni <thiwankaxvii@gmail.com>
2025-06-24 17:57:34 +0530

    changed the python file name

--- run_my_script.bat
+++ run_my_script.bat
@@ -1,5 +1,5 @@
 @echo off
 cd C:\Users\thiwa\Downloads\EY Assesment\python offSite Assesment\ey-python-assessment 
 call my_venv\Scripts\activate
-python get_apple_stock_price.py
+python apple_stock_price_tracker.py
 pause
